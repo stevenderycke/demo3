8608c12270c120b18e8217713745e484c3eadcab
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 13:07:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <?php
   ini_set('default_socket_timeout', 7); //7 seconds 
   $url = 'http://demo4:8080/';
-  $url = 'http://demo1-elia.ocp.mybpsdemo.be/';
-  $url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
-  $url = 'http://10.131.0.252:8080';
+  //$url = 'http://demo1-elia.ocp.mybpsdemo.be/';
+  //$url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
+  //$url = 'http://10.131.0.252:8080';
   echo "fetching $url: </br>";
   try {
       $results = file_get_contents($url);
