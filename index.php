74f89a35ec69eb7eabe616d6f10868f172dd9891
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 12:30:59 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Demo 3</title>
+</head>
+<body>
+<?php
+$results = file_get_contents('http://demo4/');
+echo $results;
+?>
+</body>
+</html>
