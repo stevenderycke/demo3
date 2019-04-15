984004c85bf97ca4a4a8d24e4e1bdff6c329dd9b
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 12:58:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 $url = 'http://demo4:8080/';
 $url = 'http://demo1-elia.ocp.mybpsdemo.be/';
 $url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
-$url = 'http://10.131.0.252'
+$url = 'http://10.131.0.252';
 echo "fetching $url: </br>";
 $results = @file_get_contents($url);
 echo $results;
