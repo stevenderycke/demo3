5c9327a3d8175da142cccb7c999a28b5015babe2
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 12:59:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,17 @@
 </head>
 <body>
 <?php
-$url = 'http://demo4:8080/';
-$url = 'http://demo1-elia.ocp.mybpsdemo.be/';
-$url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
-$url = 'http://10.131.0.252';
-echo "fetching $url: </br>";
-$results = @file_get_contents($url);
-echo $results;
+  $url = 'http://demo4:8080/';
+  $url = 'http://demo1-elia.ocp.mybpsdemo.be/';
+  $url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
+  $url = 'http://10.131.0.252';
+  echo "fetching $url: </br>";
+  try {
+      $results = @file_get_contents($url);
+      echo $results;
+  } catch (Exception $e) {
+      echo 'Caught exception: ',  $e->getMessage(), "\n";
+  }
 ?>
 </body>
 </html>
