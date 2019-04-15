7208b9d6f121632b57a1ecd44628310020cfc7d2
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 13:05:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 </head>
 <body>
 <?php
-  ini_set('default_socket_timeout', 10); //10 seconds 
+  ini_set('default_socket_timeout', 7); //7 seconds 
   $url = 'http://demo4:8080/';
   $url = 'http://demo1-elia.ocp.mybpsdemo.be/';
   $url = 'http://demo-different-elia-project.ocp.mybpsdemo.be/';
   $url = 'http://10.131.0.252';
   echo "fetching $url: </br>";
   try {
-      $results = @file_get_contents($url);
+      $results = file_get_contents($url);
       echo $results;
   } catch (Exception $e) {
       echo 'Caught exception: ',  $e->getMessage(), "\n";
